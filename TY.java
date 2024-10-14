28ed438839f84f9b2ace7e72f5949b03c41bd9ae
denghuixiong <youxinren918.@qq.com>
2024-10-14 19:56:13 +0800

    测试一下

---
+++ TY.java
@@ -0,0 +1,54 @@
+package Test;
+
+import BigHomework.logIN;
+
+import java.time.LocalDate;
+
+public class TY {
+    public static void main(String[] args) {
+/*
+* if (castSum < -50) {
+    moneyTxt.setText("今天已经超过50，请输入密码");
+
+    // 等待用户输入后触发验证
+    confirmButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String enteredPassword = passwordInput.getText().toString();
+
+            if (enteredPassword.equals("456")) {
+                Dao.insertOUt(money, date, textR);
+                moneyTxt.setText("支付成功");
+            } else {
+                moneyTxt.setText("密码错误");
+            }
+        }
+    });
+}
+*/
+
+
+
+
+        /*    if(castSum<-50){
+                    moneyTxt.setText("今天已经超过50，请输入密码");
+
+
+
+                    String text= moneyTxt.getText();
+                    if (text.matches("456")) {
+                        Dao.insertOUt(money ,date,textR);
+                        moneyTxt.setText("支付成功");
+                    }
+                    else {
+                        try {
+                            sleep(500);
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                        moneyTxt.setText("密码错误");
+                    }
+                }*/
+    }
+}
